fcecf0d22948b47b84cc0dee716925080b0c50d6
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-06 00:27:08 +0400

    Update LibCfgPP.cpp

--- LibCfgPP.cpp
+++ LibCfgPP.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <set>
 #include <sys/stat.h>
+#include <vector>
 
 #include "LibCfgPP.hpp"
 
@@ -8,6 +9,7 @@ using namespace LibCfgPP;
 
 struct file_info {
     std::string path;
+    std::vector<std::string> lines;
 } file_info;
 
 enum { TERMINATE_THE_PROGRAM = 1 };
@@ -26,6 +28,21 @@ void print_an_error(const std::string &message) {
         exit(EXIT_FAILURE);
 }
 
+void parse_file_lines() {
+    std::ifstream is(file_info.path);
+
+    std::string line;
+
+    while (std::getline(is, line)) {
+        file_info.lines.push_back(line);
+    }
+
+    for (const std::string &str : file_info.lines)
+        printf("%s\n", str.c_str());
+
+    is.close();
+}
+
 CfgFile::CfgFile() {}
 
 CfgFile::CfgFile(const std::string &path) { open(path); }
@@ -35,6 +52,7 @@ void CfgFile::open(const std::string &path) {
         print_an_error(
             "The file called \"" + path +
             "\" that you want to open does not exist in the specified path.");
+
     if ((path.find('.') != std::string::npos &&
          file_types.find(
              path.substr(path.find('.'), path.length() - path.find('.'))) ==
@@ -42,6 +60,10 @@ void CfgFile::open(const std::string &path) {
         (path.find('.') == std::string::npos))
         print_an_error("The type of file you are trying to open should be one "
                        "of the following: \".cfg\", \".conf\" or \".config\".");
+
+    file_info.path = path;
+
+    parse_file_lines();
 }
 
-void CfgFile::close() {}
+void CfgFile::close() { file_info.path.empty(); }
