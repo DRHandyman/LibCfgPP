32aedb3053507beb98145b2c43c7a578991980b3
Sanya <44836865+CatchMeTrippin@users.noreply.github.com>
2022-02-05 21:27:11 +0400

    Create LibCfgPP.hpp

---
+++ LibCfgPP.hpp
@@ -0,0 +1,10 @@
+#ifndef _LIBCFGPP_HPP
+#define _LIBCFGPP_HPP
+
+namespace LibCfgPP {
+const char **fileTypes = [ ".cfg", ".conf", ".config" ];
+
+class CfgFile {}
+} // namespace LibCfgPP
+
+#endif // _LIBCFGPP_HPP
