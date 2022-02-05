bdfb63acefcdcf914318e4694fdb05d3c13c6d63
Sanya <44836865+CatchMeTrippin@users.noreply.github.com>
2022-02-05 21:59:14 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "LibCfgPP.hpp"
+
+using namespace std;
+
+LibCfgPP::CfgFile cfg_file;
+
+int main() {
+    cfg_file.open("cfg_file.cfg");
+
+    cfg_file.close();
+}
