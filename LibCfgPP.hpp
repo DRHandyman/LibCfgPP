8f5af922bcb0010d1da62ddcb21fbc2217f72174
Sanya <44836865+CatchMeTrippin@users.noreply.github.com>
2022-02-05 21:33:01 +0400

    Update LibCfgPP.hpp

--- LibCfgPP.hpp
+++ LibCfgPP.hpp
@@ -4,7 +4,14 @@
 namespace LibCfgPP {
     const char **fileTypes = [ ".cfg", ".conf", ".config" ];
 
-    class CfgFile {}
+    class CfgFile {
+        public:
+            CfgFile();
+
+            void open(const char *path);
+
+            void close();
+    }
 } // namespace LibCfgPP
 
 #endif // _LIBCFGPP_HPP
