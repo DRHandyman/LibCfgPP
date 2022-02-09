c3885e0d7c9d11f6608c3d223428b11a56a6f045
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-09 22:58:52 +0400

    Create main.cpp

---
+++ test/1/main.cpp
@@ -0,0 +1,33 @@
+// A test for the time it takes to read the value of a string a million times
+
+#include <ctime>
+#include <iostream>
+
+#include "../../LibCfgPP.hpp"
+
+using namespace std;
+
+LibCfgPP::CfgFile cfg_file;
+
+int main() {
+    clock_t start, end;
+
+    start = clock();
+
+    try {
+        cfg_file.open("cfg_file.cfg");
+
+        for (int i = 0; i < 1000000; i++)
+            cfg_file.read("string");
+
+        cfg_file.close();
+    } catch (const LibCfgPP::exception &ex) {
+        cout << ex.what() << endl;
+    }
+
+    end = clock();
+
+    double time_taken = double(end - start) / double(CLOCKS_PER_SEC);
+
+    cout << time_taken << endl;
+}
