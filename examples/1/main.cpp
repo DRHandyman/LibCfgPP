6b7d020f1e55fc7888ed8fd38c67e76ef7efe98c
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-09 21:13:15 +0400

    Update main.cpp

--- examples/1/main.cpp
+++ examples/1/main.cpp
@@ -6,11 +6,17 @@ using namespace std;
 
 LibCfgPP::CfgFile cfg_file;
 
+string section_key = "section", string_key = "string";
+
 int main() {
     try {
         cfg_file.open("cfg_file.cfg");
 
-        cout << cfg_file.read("string") << endl;
+        cout << string_key + " = \"" + cfg_file.read("string") + "\"\n" << endl;
+        cout << '[' + section_key + ']' << endl
+             << "   " + string_key + " = \"" +
+                    cfg_file.read("section", "string") + '"'
+             << endl;
 
         cfg_file.close();
     } catch (const LibCfgPP::exception &ex) {
