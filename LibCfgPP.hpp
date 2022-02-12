6874f26b63b94b575e3960d92e247211f09de287
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-12 19:25:23 +0400

    Added descriptions to the functions

--- LibCfgPP.hpp
+++ LibCfgPP.hpp
@@ -9,7 +9,7 @@ namespace LibCfgPP {
 
       public:
         exception(const std::string &msg) : _msg(msg) {}
-        
+
         virtual const char *what() const noexcept override {
             return _msg.c_str();
         }
@@ -17,24 +17,54 @@ namespace LibCfgPP {
 
     class CfgFile {
       public:
+        /**
+         * @brief Main constructor
+         */
         CfgFile();
 
         CfgFile(const std::string &path);
 
+        /**
+         * @brief Opens the configuration file at the specified path.
+         * @param path - The path to a specific configuration file.
+         */
         void open(const std::string &path);
 
+        // Closes the file.
         void close();
 
+        // Returns true if the file is open, false if closed.
         bool is_open();
 
+        /**
+         * @brief Reads the value of a specific string.
+         *
+         */
         std::string read(const std::string &string_key),
+            // Reads the value of a specific string from a specific section.
             read(const std::string &section_key, const std::string &string_key);
 
+        /**
+         * @details Let's say you have made changes in the file, for example,
+         *          changed the value of a string. In order for the changes to take
+         *          effect, you will need to call this function.
+         */
         void update_file();
 
+        /**
+         * @brief Changes the value of a specific string to the one you specified.
+         * @param string_key - String key.
+         * @param value - The value to assign to the string.
+         */
         void change_the_value(const std::string &string_key,
                               const std::string &value),
-            change_the_value(const std::string &section,
+            /**
+             * @brief Changes the value of a specific string in a specific section.
+             * @param section_key - Section Key.
+             * @param string_key - String key.
+             * @param value - The value to assign to the string.
+             */
+            change_the_value(const std::string &section_key,
                              const std::string &string_key,
                              const std::string &value);
     };
