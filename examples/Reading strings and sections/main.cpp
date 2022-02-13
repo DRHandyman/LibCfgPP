1fc6d84cb8df29cb0ba630f5ce11b05355d23cdb
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-13 14:34:46 +0400

    Create main.cpp

---
+++ examples/Reading strings and sections/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "../../LibCfgPP.hpp"
+
+using namespace std;
+
+LibCfgPP::CfgFile cfg_file;
+
+int main() {
+    try {
+        cfg_file.open("cfg_file.cfg");
+
+        cout << cfg_file.read("string") << endl;
+
+        cout << cfg_file.read("section", "string") << endl;
+    } catch (const LibCfgPP::exception &ex) {
+        cout << ex.what() << endl;
+    }
+
+    cfg_file.close();
+}
