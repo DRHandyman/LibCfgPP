5a297f101c5f11bffea552cf28b5e6846cfa9fde
Sanya <99078436+DRHandyman@users.noreply.github.com>
2022-02-18 16:08:05 +0400

    Updated documentation

--- docs/html/search/functions_0.js
+++ docs/html/search/functions_0.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['cfgfile_0',['CfgFile',['../class_lib_cfg_p_p_1_1_cfg_file.html#a31d1414e228190bd584ed8ecbf3db13d',1,'LibCfgPP::CfgFile::CfgFile()=default'],['../class_lib_cfg_p_p_1_1_cfg_file.html#abf93fa83d778bac3eed0ca84523fd89c',1,'LibCfgPP::CfgFile::CfgFile(const std::string &amp;path)']]],
-  ['change_5fthe_5fvalue_1',['change_the_value',['../class_lib_cfg_p_p_1_1_cfg_file.html#a9a8680c8adbaa7460723c7c75cf6874f',1,'LibCfgPP::CfgFile::change_the_value(const std::string &amp;string_key, const std::string &amp;value)'],['../class_lib_cfg_p_p_1_1_cfg_file.html#a5bfd6a7313ef7160b35484693d6310aa',1,'LibCfgPP::CfgFile::change_the_value(const std::string &amp;section_key, const std::string &amp;string_key, const std::string &amp;value)']]],
+  ['change_1',['change',['../class_lib_cfg_p_p_1_1_cfg_file.html#a6cf5dc012ca450b660aa0f0bbff81fc3',1,'LibCfgPP::CfgFile::change(const std::string &amp;string_key, const std::string &amp;value)'],['../class_lib_cfg_p_p_1_1_cfg_file.html#afe2f1bf9268706f07e90803b7e8ac538',1,'LibCfgPP::CfgFile::change(const std::string &amp;section_key, const std::string &amp;string_key, const std::string &amp;value)']]],
   ['close_2',['close',['../class_lib_cfg_p_p_1_1_cfg_file.html#adeed2a2abfa778117d9f2f724624ffc7',1,'LibCfgPP::CfgFile']]],
   ['create_5fstring_3',['create_string',['../class_lib_cfg_p_p_1_1_cfg_file.html#a114d575f44a79c77a7961f9d2cb60ef5',1,'LibCfgPP::CfgFile']]]
 ];
